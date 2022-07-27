261a02b0e7cbbd917f15efa1dd88a701be966c1c
Oyinkansola Ojutiku <oyinkanojutiku@gmail.com>
2022-07-26 20:28:22 -0400

    responsive nav

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleSideMenu() {
+    sideMenu = document.getElementById('sideMenu');
+    burger = document.getElementById('hamburger');
+    sideMenu.classList.toggle("open");
+    burger.classList.toggle("open");
+}
