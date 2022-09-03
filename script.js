70ce9b86b50acfa7e1ba2e558c7bfc755b53ec85
Oyinkansola Ojutiku <oyinkanojutiku@gmail.com>
2022-09-03 02:51:23 -0400

    Add light/dark mode toggle

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+
+    overlay = document.getElementById('overlay');
+    if (themeName == 'dark') {
+        overlay.classList.add('dark');
+        overlay.classList.remove('light');
+    } else {
+        overlay.classList.remove('dark');
+    }
+
+    localStorage.setItem('theme', themeName);
+    document.documentElement.setAttribute("data-theme", themeName);
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'dark') {
+        setTheme('light');
+    } else {
+        setTheme('dark');
+    }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'dark') {
+        setTheme('dark');
+    } else {
+        setTheme('light');
+    }
+})();
+
 function toggleSideMenu() {
     sideMenu = document.getElementById('sideMenu');
     burger = document.getElementById('hamburger');
